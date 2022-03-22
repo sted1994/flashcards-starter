ac696b37bd600ccb976b1f998d30c1aa6fa49515
Stephen Tedesco <s_tylert@icloud.com>
2022-03-21 19:41:38 -0600

    Finish Deck tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,14 +6,22 @@ const Deck = require('../src/Deck');
 
 describe('Deck', function(){
 
-  it('Should start with an array where cards will be added', function(){
-    let deck = new Deck();
+  it('Should start with an array of cards', function(){
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards)
 
     expect(deck.cards).to.be.a('array');
   })
 
   it('Should know how many cards in the deck (cards array)', function(){
-    var deck = new Deck();
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards)
 
     expect(deck.countCards()).to.equal(deck.cards.length)
   })
